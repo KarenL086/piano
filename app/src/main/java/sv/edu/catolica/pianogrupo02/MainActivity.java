13a3db39b6a9a2848e36d75dc68b97cfbf9314d6
Karen <karenlm086@gmail.com>
2023-10-04 22:05:31 -0600

    Inicial

---
+++ app/src/main/java/sv/edu/catolica/pianogrupo02/MainActivity.java
@@ -0,0 +1,33 @@
+package sv.edu.catolica.pianogrupo02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sonar (MediaPlayer sonido){
+        if(sonido.isPlaying()){
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            } catch (IllegalStateException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void SonarDo(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.do1);
+        sonar(sonido);
+    }
+}
