e2b977876b945de0a8d3a2419498bcd59a1ea3b9
Karen <karenlm086@gmail.com>
2023-10-06 00:44:00 -0600

    met

--- app/src/main/java/sv/edu/catolica/pianogrupo02/pAnimales.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo02/pAnimales.java
@@ -2,7 +2,10 @@ package sv.edu.catolica.pianogrupo02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class pAnimales extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class pAnimales extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panimales);
+    }
+    public void sonar (MediaPlayer sonido){
+        if(sonido.isPlaying()){
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            } catch (IllegalStateException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void SonarElefante(View view) {
+        MediaPlayer s;
+    }
+
+    public void SonarAve(View view) {
+        MediaPlayer s;
+    }
+
+    public void SonarMono(View view) {
+        MediaPlayer s;
+    }
+
+    public void SonarSerpiente(View view) {
+        MediaPlayer s;
     }
 }
