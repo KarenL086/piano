94163b825c7484ac1c2f670edc66b7f60f30cf65
Verónica Marín <veronica.marin@catolica.edu.sv>
2023-10-06 22:36:11 -0600

    Cambios en el diseño

--- app/src/main/java/sv/edu/catolica/pianogrupo02/pIntrumentos.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo02/pIntrumentos.java
@@ -2,7 +2,9 @@ package sv.edu.catolica.pianogrupo02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class pIntrumentos extends AppCompatActivity {
 
@@ -11,4 +13,58 @@ public class pIntrumentos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pintrumentos);
     }
+
+    public void sonar (MediaPlayer sonido){
+        if(sonido.isPlaying()){
+            sonido.stop();
+        }else{
+            try {
+                sonido.start();
+            } catch (IllegalStateException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void SonarTambor(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.tambor);
+        sonar(sonido);
+    }
+
+    public void SonarTrompeta(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.trompeta);
+        sonar(sonido);
+    }
+
+    public void SonarFlauta(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.dulce);
+        sonar(sonido);
+    }
+
+    public void SonarGuitarra(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.guitarra);
+        sonar(sonido);
+    }
+
+    public void SonarViolin(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.violin);
+        sonar(sonido);
+    }
+
+    public void SonarSaxo(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.saxofon);
+        sonar(sonido);
+    }
+
+    public void SonarArpa(View view) {
+        MediaPlayer sonido = MediaPlayer.create(this, R.raw.harpa);
+        sonar(sonido);
+    }
+
+
 }
+
+
+
+
+
